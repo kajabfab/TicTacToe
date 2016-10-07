a2d6c0dbe58fec134346e7452a43e8f4ac236021
kajabfab <vishal.bfab@gmail.com>
2016-10-07 23:30:26 +0530

    #2 controller and design

--- controller.js
+++ controller.js
@@ -1,3 +1,26 @@
-/**
- * Created by stacker on 7/10/16.
- */
+angular.module("game",
+    [])
+
+.controller("play",['$scope',function ($scope) {
+
+    $scope.turn = 1; // 1 -> P1's turn; 2 -> P2's turn
+
+    $scope.whosturn = function () {
+        return $scope.turn;
+    };
+
+    $scope.mark = function (event) {
+        var box = event.target;
+        if($scope.turn == 1)
+        {
+            box.src = "x.png";
+            $scope.turn = 2;
+        }
+        else if($scope.turn == 2)
+        {
+            box.src = "o.png";
+            $scope.turn = 1;
+        }
+    }
+
+}]);
